b3aa7cc2a12d62ef3f33116a7eee00c42cd1aa6b
ayushi501 <116541983+ayushi501@users.noreply.github.com>
2024-07-17 13:13:47 +0530

    Add files via upload

---
+++ Greedy/MInimum Cost To Cut The Cake 1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minimumCost(self, m: int, n: int, horizontalCut: list[int], verticalCut: list[int]) -> int:
+        horizontalCut.sort(reverse=True)
+        verticalCut.sort(reverse=True)
+        i, j = 0, 0
+        n = len(horizontalCut)
+        m = len(verticalCut)
+        cost = 0
+        hPieces, vPieces = 1, 1
+        while i < n and j < m:
+            if horizontalCut[i] >= verticalCut[j]:
+                cost += horizontalCut[i] * vPieces
+                hPieces += 1
+                i += 1
+            elif horizontalCut[i] < verticalCut[j]:
+                cost += verticalCut[j] * hPieces
+                vPieces += 1
+                j += 1
+        while i < n:
+            cost += horizontalCut[i] * vPieces
+            i += 1
+        while j < m:
+            cost += verticalCut[j] * hPieces
+            j += 1
+        return cost
