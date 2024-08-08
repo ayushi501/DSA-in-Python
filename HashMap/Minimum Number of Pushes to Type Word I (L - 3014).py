57738fca768b0f7ed3199df16105f6a66c721b76
ayushi501 <116541983+ayushi501@users.noreply.github.com>
2024-08-08 06:52:11 +0530

    Update Minimum Number of Pushes to Type Word I (L - 3014).py

--- HashMap/Minimum Number of Pushes to Type Word I (L - 3014).py
+++ HashMap/Minimum Number of Pushes to Type Word I (L - 3014).py
@@ -1,12 +1,10 @@
-T.C. = O(nlogn)
-S.C. = O(n)
+T.C. = O(n)
+S.C. = O(1)
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        c = Counter(word)
-        l = list(sorted(c.items(), key = lambda x : x[1])) #O(nlogn)
-        i = len(l) - 1
         ans = 0
-        while i >= 0:                                      #O(n)
-            ans += ((i//8+1) * l[i][1])
-            i -= 1
+        i = 0
+        while i < len(word):    #O(n)
+            ans += i//8 + 1
+            i += 1
         return ans
